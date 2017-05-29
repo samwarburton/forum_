d7458b19727e433c5b293a39e932b60e39f2e9c3
Sam Warburton <samwarburton93@gmail.com>
2017-05-29 23:16:32 +1000

    Edited RegisterController so that it sets default user role to 'user'. User now has a fillable field called 'role' and the user table now has an appropriate field for 'role'

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role',
     ];
 
     /**
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-
+    
 
 
     public function threads(){
