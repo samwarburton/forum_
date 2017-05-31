c4b9b9c91af7e406ba35bce560204242d79a2e2d
Sam Warburton <samwarburton93@gmail.com>
2017-05-31 17:07:00 +1000

    Made significant design improvements to display threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,12 +2,19 @@
 
 
 @section('content')
-<div class="col-md-8 col-md-offset-2">
+<div class="col-md-10 col-md-offset-1">
 
 	<div class="panel panel-default">
-	<div class="panel-heading">{{$thread->title}}</div>
+	<div class="panel-heading">Created {{$thread->created_at->format(' h:i a, D M y')}}</div>
 	<div class="panel-body">
+	<div class="col-md-2">
+	<img class="img img-responsive" src="https://freeiconshop.com/wp-content/uploads/edd/person-flat.png">
+	<h3><a href="">{{$thread->user->name}}</a></h3>
+	<p>Joined: {{$thread->user->created_at->format('d M y')}}</p>
+	</div>
+	<div class="col-md-9 col-md-offset-1">
 	<p>{{$thread->body}}</p>
+	</div>
 	</div>
 </div>
 
@@ -27,15 +34,32 @@
 @foreach($thread->comments as $comment)
 	
 	<div class="panel panel-default">
-	<div class="panel-heading">{{$comment->user->name}}</div>
+	<div class="panel-heading">Posted {{$comment->created_at->format(' h:i a, D M y')}}
+	</div>
 	<div class="panel-body">
-	<p>{!! $comment->body !!}</p>
-	@if($comment->user->id == Auth::id())
+	<div class="col-md-2">
+	<img class="img img-responsive" src="https://freeiconshop.com/wp-content/uploads/edd/person-flat.png">
+	<h3><a href="">{{$comment->user->name}}</a></h3>
+	<p>Joined: {{$comment->user->created_at->format('d M y')}}</p>
+	</div>
 
-	<p>User can edit this</p>
+	<div class="col-md-9 col-md-offset-1">
+	
+	<p>{!! $comment->body !!}</p>
+
+
+
+	</div>
+	</div>
+	 <div class="panel-footer">Report
+	 	@if($comment->user->id == Auth::id())
+
+	<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+	<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+
 
 	@endif
-	</div>
+	 </div>
 </div>
 
 
